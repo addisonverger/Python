627aaef1cae78b242b3ea7f20a489ee1ce2c1623
Addison Verger <addison.j.verger@gmail.com>
2023-06-02 18:36:32 -0400

    Update HackerRank.py

--- HackerRank.py
+++ HackerRank.py
@@ -133,3 +133,19 @@ if __name__ == '__main__':
             sls_students.append(x[0])
 
     print("\n".join(sorted(sls_students))) 
+
+# -- Finding the percentage
+
+if __name__ == '__main__':
+    n = int(input())
+    student_marks = {}
+    for _ in range(n):
+        name, *line = input().split()
+        scores = list(map(float, line))
+        student_marks[name] = scores
+    query_name = input()
+
+    lst = student_marks[query_name]
+    avg_lst = sum(lst) / len(lst)
+    rnd_avg = format(round(avg_lst, 2), '.2f')
+    print (rnd_avg)
