78e4077317393db733463ac2c3cb582a1725553f
Addison Verger <addison.j.verger@gmail.com>
2023-06-02 14:23:31 -0400

    Update HackerRank.py

--- HackerRank.py
+++ HackerRank.py
@@ -84,3 +84,21 @@ while i <= n:
 str_list = [str(x) for x in list]
 
 print("".join(str_list))
+
+# -- List Comprehensions
+
+if __name__ == '__main__':
+    x = int(input())
+    y = int(input())
+    z = int(input())
+    n = int(input())
+    
+coords=[]
+
+for i in range(x + 1): 
+    for j in range(y + 1): 
+        for k in range(z + 1): 
+            if ((i + j + k) != n):
+                coords.append([i,j,k])
+                    
+print(coords)
