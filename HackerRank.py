84b78ef29b8184f208a4fcef9021382f8e6edd15
Addison Verger <addison.j.verger@gmail.com>
2023-06-04 17:44:13 -0400

    Update HackerRank.py

--- HackerRank.py
+++ HackerRank.py
@@ -186,3 +186,16 @@ for _ in range(T):
         print(True)
     except re.error:
         print(False)
+
+# -- String Split and Join
+
+def split_and_join(line):
+    # write your code here
+    line = line.split(" ")
+    line = "-".join(line)
+    return line
+    
+if __name__ == '__main__':
+    line = input()
+    result = split_and_join(line)
+    print(result)
