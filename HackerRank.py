6d07c489543fe065257d9d804d2e052e07c66180
Addison Verger <addison.j.verger@gmail.com>
2023-06-23 16:28:52 -0400

    Update HackerRank.py

--- HackerRank.py
+++ HackerRank.py
@@ -637,3 +637,18 @@ if __name__ == '__main__':
     arr = list(map(int, input().split()))
     result = average(arr)
     print(result)
+
+# -- Symmetric Difference
+
+M = int(input())
+m = set(map(int, input().split()))
+N = int(input())
+n = set(map(int, input().split()))
+
+a = m.difference(n)
+b = n.difference(m)
+c = list(a.union(b))
+c.sort()
+
+for x in range(len(c)):
+    print(c[x])
